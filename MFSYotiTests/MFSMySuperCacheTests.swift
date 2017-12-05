c24e32939ffdea7c4a8a43c8a0f2f138ed3b51dd
Nilofar Vahab poor <nelly.vahab@gmail.com>
2017-12-05 17:35:23 +0000

    Updating Unit Tests

--- MFSYotiTests/MFSMySuperCacheTests.swift
+++ MFSYotiTests/MFSMySuperCacheTests.swift
@@ -10,7 +10,8 @@ import XCTest
 
 class MFSMySuperCacheTests: XCTestCase {
     var cache: CacheOrganiser = CacheOrganiser()
-
+    var baseMaxAge:Double = 6400
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -27,7 +28,7 @@ class MFSMySuperCacheTests: XCTestCase {
         XCTAssertNil(cache, "cache should be nil after deinit")
     }
 
-    func testThatCacheOrganiserCanAddImageWithIdentifierAndMaxAge() {
+    func testThatCacheOrganiserCanAddImageWithIdentifierAndMaxAge(){
         // Given
         let image = UIImage(named: "Refresh-icon")
         let identifier = "refresh"
@@ -40,15 +41,30 @@ class MFSMySuperCacheTests: XCTestCase {
             cachedImage = image!
         }
         // Then
+        XCTAssertNotNil(cache.imagesCache, "cache.imagesCache should not be nil")
         XCTAssertNotNil(cachedImage, "cached image should not be nil")
     }
+    
+    func testThatCacheCanFindImageWithIdentifier() {
+        // Given
+        let image = UIImage(named: "Refresh-icon")
+        let identifier = "refresh"
+        
+        // When
+        cache.add(image: image!, withIdentifier: identifier, maxAge: baseMaxAge)
+        let cachedImage = cache.findImageWithId(imageID: identifier)
+        XCTAssertNotNil(cache.imagesCache, "cache.imagesCache should not be nil")
+        XCTAssertNotNil(cachedImage, "cached image should not be nil")
+
+    }
+    
     func testThatCacheCanRemoveImageWithIdentifier() {
         // Given
         let image = UIImage(named: "Refresh-icon")
         let identifier = "refresh"
         
         // When
-        cache.add(image: image!, withIdentifier: identifier, maxAge: 6400)
+        cache.add(image: image!, withIdentifier: identifier, maxAge: baseMaxAge)
         let cachedImageExists = cache.findImageWithId(imageID: identifier) != nil
         let removedImage = cache.removeImageFromCache(identifier: identifier)
         let checkIfImageExistsInCache = cache.findImageWithId(imageID: identifier) != nil
